602a59dbcda92d16288a2583fa4131d5e45986dd
Hemanth B <hemanth22hemu@gmail.com>
2024-03-24 13:22:49 +0530

    Update batchjob1.py

--- batchjob1.py
+++ batchjob1.py
@@ -7,4 +7,18 @@ headers = {}
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+paydata = response.json()
+
+def print_dict_and_list_recursive(data, indent=0):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            print('  ' * indent + f'{key}:')
+            print_dict_and_list_recursive(value, indent + 2)
+    elif isinstance(data, list):
+        for index, item in enumerate(data):
+            print('  ' * indent + f'[{index}]:')
+            print_dict_and_list_recursive(item, indent + 2)
+    else:
+        print('  ' * indent + f'{data}')
+
+print_dict_and_list_recursive(paydata)
