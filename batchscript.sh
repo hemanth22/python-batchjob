69366e34385b6d5b4f4e2328aca86142dd7ddf40
hemanth22 <hemanth22hemu@gmail.com>
2024-03-26 09:59:52 +0000

    feat: batchscript move script to tmp

--- batchscript.sh
+++ batchscript.sh
@@ -3,16 +3,26 @@ echo "[TASK 1] Install python modules from requirements files"
 python3 -m pip install --no-cache-dir -r requirements.txt
 echo "[TASK 2] Print Python3 Verions"
 python3 -V
-touch batchjob1.txt
-chmod 755 batchjob1.txt
-touch batchjob1.html
-chmod 755 batchjob1.html
-echo "[TASK 2.1] Configure python and python3"
+echo "[TASK 2.2] Copy file to tmp directory"
+cp -v requirements.txt /tmp
+cp -v README.md /tmp
+cp -v inventory /tmp
+cp -v converter_html.awk /tmp
+cp -v batchjob.playbook /tmp
+cp -v batchjob1.py /tmp
+cp -v ansible.cfg /tmp
+cp -v batchscript.sh /tmp
+cp -v mail_batchjob1.py /tmp
+cp -v Dockerfile /tmp
+echo "[TASK 2.2] Configure python and python3"
 alternatives --set python3 /usr/bin/python3.9
 alternatives --set python /usr/bin/python3.9
 echo "[TASK 3] Executing Batch Jobs"
+cd /tmp/
 python3 batchjob1.py > batchjob1.txt
 echo "[TASK 4] Transform output to html format"
+cd /tmp/
 cat batchjob1.txt | awk -f converter_html.awk > batchjob1.html
 echo "[TASK 5] Mail the output"
+cd /tmp/
 python3 mail_batchjob1.py
