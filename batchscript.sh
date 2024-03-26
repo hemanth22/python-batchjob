4c9280da01121d36f450dbfa5439f7da77f134c3
Hemanth B <hemanth22hemu@gmail.com>
2024-03-26 13:34:30 +0530

    Update batchscript.sh

--- batchscript.sh
+++ batchscript.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -xv
 echo "[TASK 1] Install python modules from requirements files"
-python3 -m pip install -r requirements.txt
+python3 -m pip install --no-cache-dir -r requirements.txt
 echo "[TASK 2] Print Python3 Verions"
 python3 -V
+touch /tmp/batchjob1.txt
+chmod 755 /tmp/batchjob1.txt
+touch /tmp/batchjob1.html
+chmod 755 /tmp/batchjob1.html
 echo "[TASK 3] Executing Batch Jobs"
-python3 batchjob1.py > batchjob1.txt
+python3 batchjob1.py > /tmp/batchjob1.txt
 echo "[TASK 4] Transform output to html format"
-cat batchjob1.txt | awk -f converter_html.awk > batchjob1.html
+cat /tmp/batchjob1.txt | awk -f converter_html.awk > /tmp/batchjob1.html
 echo "[TASK 5] Mail the output"
 python3 mail_batchjob1.py
