2a59baeb59d457e02a67c8d6f4dc66f0ee33c930
hemanth22 <hemanth22hemu@gmail.com>
2024-03-26 10:12:46 +0000

    feat: batch script scrpit corrections

--- batchscript.sh
+++ batchscript.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -xv
-echo "[TASK 1] Install python modules from requirements files"
-python3 -m pip install --no-cache-dir -r requirements.txt
-echo "[TASK 2] Print Python3 Verions"
-python3 -V
-echo "[TASK 2.2] Copy file to tmp directory"
+echo "[TASK 1] Copy file to tmp directory"
 cp -v requirements.txt /tmp
 cp -v README.md /tmp
 cp -v inventory /tmp
@@ -14,9 +10,8 @@ cp -v ansible.cfg /tmp
 cp -v batchscript.sh /tmp
 cp -v mail_batchjob1.py /tmp
 cp -v Dockerfile /tmp
-echo "[TASK 2.2] Configure python and python3"
-alternatives --set python3 /usr/bin/python3.9
-alternatives --set python /usr/bin/python3.9
+echo "[TASK 2] Print Python3 Verions"
+python3 -V
 echo "[TASK 3] Executing Batch Jobs"
 cd /tmp/
 python3 batchjob1.py > batchjob1.txt
